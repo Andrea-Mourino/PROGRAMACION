eafc19e52caee06ea4fee4ac56f52b97748c3700
Andrea Mourino <amourinodesousa@danielcastelao.org>
2024-10-23 10:47:04 +0200

    Comentario

--- Multiplosde7-100/src/SoliciteN.java
+++ Multiplosde7-100/src/SoliciteN.java
@@ -1,17 +1,28 @@
 import java.util.Scanner;
 
 /**
- * Hacer un bucle con numeros primos
+ * Hacer un bucle con numeros primos entre N
  * @author Andrea Mouriño
  * @version 1.1
  */
 
 public class SoliciteN {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        for(int i= 2; i= i % 2) {
-            System.out.println (i + " es un numero primo");
+        int n;
+        boolean primo=true;
+            Scanner sc = new Scanner(System.in);
+            n= sc.nextInt();
+            for (int i=2; i<=n; i++) //Todos los numeros entre 1 y N que vamos a ver si son divisibles
+            {
+            for(int j=2; j<i; j++) //Los numeros por los que dividimos
+            {
+                if (i%j==0) { //Es ver si i es divisible entre j
+                    primo=false;
+                }
+            }
+                if(primo==true)
+                {System.out.println(i);}
+                    primo=true;
         }
     }
 }
