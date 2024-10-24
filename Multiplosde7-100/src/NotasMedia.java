affc157982e2f8e1e7932ccdd4ba18bd6256968b
Andrea Mourino <amourinodesousa@danielcastelao.org>
2024-10-24 13:22:11 +0200

    añadir do y ; en while

--- Multiplosde7-100/src/NotasMedia.java
+++ Multiplosde7-100/src/NotasMedia.java
@@ -12,16 +12,17 @@ public class NotasMedia {
         int nota,suma,numero,media;
         suma=0;
         numero=0;
-        System.out.println("introduce tu nota para la media, para salir -1: ");
-        nota= sc.nextInt();
+        do {
+            System.out.println("introduce tu nota para la media, para salir -1: ");
+            nota = sc.nextInt();
+            suma=suma+nota;
+            numero++;
+        }
 
-            while(nota!=-1) {
-                suma=suma+nota;
-                numero++;
-                nota= sc.nextInt();
-            }
-                media=suma/numero;
-                System.out.println("su media es de: " + media);
+        while(nota!=-1);
+
+        media=suma/numero;
+        System.out.println("su media es de: " + media);
 
     }
 }
