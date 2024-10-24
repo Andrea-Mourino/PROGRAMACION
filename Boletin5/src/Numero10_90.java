0fc76ac3f69d3ec5f2e17499bc52f6b38efdf70f
Andrea Mourino <amourinodesousa@danielcastelao.org>
2024-10-24 14:34:57 +0200

    <=90

--- Boletin5/src/Numero10_90.java
+++ Boletin5/src/Numero10_90.java
@@ -6,17 +6,18 @@ import java.util.Scanner;
  * @version 1.1
  */
 
-public class Main {
+public class Numero10_90 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int valor1= 0;
+        int suma= 0;
+        long produto= 0; //long para evitar desbordamiento al tener mas bits que int
 
-        for (int i= 10; i < 90; i++) {
-
-
+        for (int i= 10; i <= 90; i++) { //aqui estoy diciendo que el valor i puede ser igual a 10 y menos que 90 que sume uno
+            suma += i;        // Sumamos el número actual
+            produto *= i;    // Multiplicamos el número actual
         }
-
-
+        System.out.println("La suma de los números entre 10 y 90 es: " + suma);
+        System.out.println("El producto de los números entre 10 y 90 es: " + produto);
     }
 }
