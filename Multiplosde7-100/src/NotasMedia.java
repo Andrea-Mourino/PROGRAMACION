7f8948f565d995df58b72674c62cef7cf3d8ca0e
Andrea Mourino <amourinodesousa@danielcastelao.org>
2024-10-24 13:37:09 +0200

    meter double

--- Multiplosde7-100/src/NotasMedia.java
+++ Multiplosde7-100/src/NotasMedia.java
@@ -9,20 +9,18 @@ import java.util.Scanner;
 public class NotasMedia {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int nota,suma,numero,media;
-        suma=0;
-        numero=0;
+        double sumaNotas= 0.0, nota= 0.0;
+        int cantidade= 0;
         do {
             System.out.println("introduce tu nota para la media, para salir -1: ");
-            nota = sc.nextInt();
-            suma=suma+nota;
-            numero++;
-        }
+            nota = sc.nextDouble(); //porque notas es double
+            if (nota != -1) {
+                sumaNotas= sumaNotas + nota;
+                cantidade++;
+            }
+        } while(nota!=-1);
 
-        while(nota!=-1);
-
-        media=suma/numero;
-        System.out.println("su media es de: " + media);
+        System.out.println("su media es de: " + sumaNotas/cantidade);
 
     }
 }
