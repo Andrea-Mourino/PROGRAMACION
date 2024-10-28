715ee525b69c5666cc98063498f7cf97cb4446b9
Andrea Mourino <amourinodesousa@danielcastelao.org>
2024-10-28 12:59:48 +0100

    nueva tarea

---
+++ Boletin5/src/AreaRectangulo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Realiza un programa que calcule a suma e o produto dos números comprendidos entre 10 e 90.
+ * @author Andrea Mouriño
+ * @version 1.1
+ */
+
+public class AreaRectangulo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+}
