2411eef9b508f615ab8fd627a2a1b7d3c5a7a75a
Andrea Mourino <amourinodesousa@danielcastelao.org>
2024-10-28 13:43:58 +0100

    do while

---
+++ Boletin5/src/TablaMultiplicar.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Codifica un programa que solicite un número e, visualice a táboa de multiplicar dese número. O programa rematara cando prememos o cero.
+ * @author Andrea Mouriño
+ * @version 1.1
+ */
+
+public class TablaMultiplicar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
