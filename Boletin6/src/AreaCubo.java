0d2b2d7b391b2896e4ebca981d5e19677a92acff
Andrea Mourino <amourinodesousa@danielcastelao.org>
2024-11-04 14:03:56 +0100

    haciendo un for

---
+++ Boletin6/src/AreaCubo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Realizar unha función que mostre a área dun cubo segundo se lle especifique. Para distinguir o caso se lle pasará a opción cun número 1 (área) ou 2 (volume). Ademáis, hai que pasarlle a lonxitude do lado.
+ * @author Andrea Mouriño
+ * @version 1.1
+ */
+
+public class AreaCubo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int area= 0;
+        int volume= 0;
+
+        for (int i= 0; i<area; i++) {
+
+
+        }
+
+
+    }
+}
