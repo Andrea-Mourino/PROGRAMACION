c378d259a06de78dd51a9c99c96932c4d3bbe830
Andrea Mourino <amourinodesousa@danielcastelao.org>
2024-11-04 12:38:50 +0100

    terminado

--- Boletin5/src/TablaMultiplicar.java
+++ Boletin5/src/TablaMultiplicar.java
@@ -9,7 +9,19 @@ import java.util.Scanner;
 public class TablaMultiplicar {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int numero, n2;
+        int seguir=1;
+        int veces=1;
+        System.out.println("dime un numero para que te muestre su tabla de multiplicar: ");
+        Scanner sc = new Scanner(System.in);
+        numero= sc.nextInt();
+        while (seguir!=0){
+            n2=veces*numero;
+            veces++;
+            System.out.println(n2);
+            System.out.println("para seguir salir pulse 0, para seguir cualquier otro numero: ");
+            seguir= sc.nextInt();
+        }
 
     }
 }
