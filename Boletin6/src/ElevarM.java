9311913db3a3c08e0a1a97256be54faade684d0d
Andrea Mourino <amourinodesousa@danielcastelao.org>
2024-11-06 09:50:14 +0100

    agregar comentario

---
+++ Boletin6/src/ElevarM.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Escribe a función que resolva o resultado de elevar n veces o número m (mn) de forma recursiva.
+ * @author Andrea Mouriño
+ * @version 1.1
+ */
+
+public class ElevarM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+    static int elevarNumero(int m, int m2) { //el estatic es para mantenerlo estatico
+        if (m2==0) { //el m2 seria el numero elevado al m
+            return 1; //es para devolverme el uno
+        }
+        return m * elevarNumero(m,m2-1);
+    }
+
+
+}
