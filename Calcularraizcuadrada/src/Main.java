b76cca88e253840de6dd16388cbbaf9f4fdd4de8
Andrea Mourino <amourinodesousa@danielcastelao.org>
2024-11-14 14:07:24 +0100

    raiz cuadrada

---
+++ Calcularraizcuadrada/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int RaizCuadrada= 23;
+        int multiplicar= 0;
+        if (RaizCuadrada ==1) {
+            System.out.println("La raiz cuadrada de 23 es exacta!");
+
+        }
+        else {
+            System.out.println("La raiz cuadrada de 23 no es exacta y este es el resto: ");
+
+        }
+    }
+}
