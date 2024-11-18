5301a75ac0ab051adc047a1e5ea58e6346f76ce1
Andrea Mourino <amourinodesousa@danielcastelao.org>
2024-11-18 14:27:13 +0100

    comentario

--- Calcularraizcuadrada/src/Main.java
+++ Calcularraizcuadrada/src/Main.java
@@ -2,17 +2,27 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner;
 
+/**
+Calcula la raiz cuadrada dun numero natural mediante aproximacionhs. NO caso de que non se4xa exacta , mostrar o resto.
+@author Andrea Mouriño
+@version
+ */
+
 public class Main {
     public static void main(String[] args) {
         int RaizCuadrada= 23;
         int multiplicar= 0;
-        if (RaizCuadrada ==1) {
-            System.out.println("La raiz cuadrada de 23 es exacta!");
 
+        for (int i=0; i<10; i++);
+        for (int i=0; i<5; i++);
+
+        if (RaizCuadrada == 1) {
+            System.out.println("La raiz cuadrada de 23 es exacta!");
         }
         else {
-            System.out.println("La raiz cuadrada de 23 no es exacta y este es el resto: ");
-
+            if (RaizCuadrada * multiplicar) {
+                System.out.println("La raiz cuadrada de 23 no es exacta y este es el resto: ");
+            }
         }
     }
 }
