a4bab340ce3f885973c1a2b10c6149fd802541c1
Andrea Mourino <amourinodesousa@danielcastelao.org>
2024-11-20 10:48:14 +0100

    comentario

---
+++ Boletin7/src/NotasmoduloDAM.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**Carga un array de tipo int, coas notas do módulo de programación, dos 30 alumnos da clase de DAM .
+- Visualiza o numero de 	aprobados e o de suspensos.
+- Calcula e visualiza a nota media da clase.
+- Visualiza a nota mais alta.
+ @author Andrea Mouriño
+ @version 1.0
+**/
+
+
+public class NotasmoduloDAM {
+    public static void main(String[] args) {
+
+
+    }
+}
