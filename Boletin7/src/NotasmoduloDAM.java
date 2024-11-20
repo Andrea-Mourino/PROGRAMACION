f6e9dd7ebfd2a158adb72a1e47f42217fcdf6467
Andrea Mourino <amourinodesousa@danielcastelao.org>
2024-11-20 11:04:07 +0100

    for agregado

--- Boletin7/src/NotasmoduloDAM.java
+++ Boletin7/src/NotasmoduloDAM.java
@@ -11,7 +11,19 @@ import java.util.Scanner;
 
 public class NotasmoduloDAM {
     public static void main(String[] args) {
+        int numeroAprobados =0, numeroSuspensos=0;
+        int[] notas = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                2, 3, 4, 5, 6, 7, 8, 9, 10, 1,
+                3, 4, 5, 6, 7, 8, 9, 10, 1, 2};
 
+        for(int i=0; i<notas.length; i++){
+
+            if(notas[i]<5){
+               numeroSuspensos++;
+            }
+        }
+
+        for(int j=0; j)
 
     }
 }
