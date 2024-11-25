95c99f0c895db65ed891b4f4ecfa21b2bd941da0
Andrea Mourino <amourinodesousa@danielcastelao.org>
2024-11-25 12:30:45 +0100

    mitad

--- untitled/src/pedazotabla.java
+++ untitled/src/pedazotabla.java
@@ -2,7 +2,7 @@ public class pedazotabla {
     public static void main(String[] args) {
         int[] tabla = {5, 7, 9, 15, 25, 90, 101};
 
-        int UnaParte = tabla.length / ;
+        int UnaParte = tabla.length /2 ;
 
         System.out.println("La mitad de la tabla es:");
         for (int i = 0; i < UnaParte; i++) {
