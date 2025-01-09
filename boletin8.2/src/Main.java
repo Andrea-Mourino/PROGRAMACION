6c1962585de4d3ed723da5566559af6200d5e8fe
Andrea Mourino <amourinodesousa@danielcastelao.org>
2025-01-09 13:40:14 +0100

    substring

---
+++ boletin8.2/src/Main.java
@@ -0,0 +1,35 @@
+/**
+ * Escribir funcións que dada unha cadena de caracteres:
+ * Imprima os dous primeiros caracteres.
+ *
+ * Imprima os tres últimos caracteres.
+ *
+ * Imprima dita cadea cada dous caracteres. Ex.: recta debería imprimir rca
+ *
+ * Imprima a cadea nun sentido e en sentido inverso. Ex: reflexo imprime reflexooxelfer.
+ *
+ * @author Andrea Mouriño
+ * @version1.0
+ */
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        String nome= "Kuromi";
+        int tamaño= c
+        if (nome.length() >=2) {
+            System.out.println("Dos primeros caracteres: " + nome.substring(0,2));
+        }
+
+        if (nome.length() >=3) {
+            System.out.println("Tres ultimos caracteres: " + nome.substring(nome.length() -3));
+        }
+
+        for (int i=0; i<=2; i++) {
+
+        }
+
+    }
+}
