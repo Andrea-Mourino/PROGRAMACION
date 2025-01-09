8cd0c4bc0c0628eb805838ff66771426ecba0b6b
Andrea Mourino <amourinodesousa@danielcastelao.org>
2025-01-09 14:27:53 +0100

    cadea

---
+++ boletin8.2/src/cadea_caracteres.java
@@ -0,0 +1,19 @@
+/**
+Inserte o caracter entre cada letra da cadea. Ex: separar e ‘,’ debería devolver s,e,p,a,r,a,r
+Reemplace tódolos espazos polo caracter. Ex: ‘meu arquivo de texto.txt’ e ‘\_’ debería devoltar ‘meu\_arquivo\_de\_texto.txt’
+Reemplace tódolos díxitos na cadea polo caracter. Ex: súa clave é: 1540 e ‘X’ debería devotar súa clave é: XXXX
+Inserte o caracter cada 3 díxitos na cadea. Ex. 2552552550 e ‘.’ debería devoltar 255.255.255.0
+
+ @author Andrea Mouriño
+ @version 1.0
+*/
+
+
+public class cadea_caracteres {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
