114ba81ab1c0fd5fffe5872045da8a36666e154d
Andrea Mourino <amourinodesousa@danielcastelao.org>
2025-02-19 14:31:49 +0100

    cuewnta corriente

---
+++ Boletin_9/src/cuentacorriente.java
@@ -0,0 +1,15 @@
+public class cuentacorriente {
+    private String numerocuenta;
+    private String nif;
+    private String nombret;
+    private double saldo;
+    public static int contadorCuentas;
+
+
+
+
+
+
+
+
+}
