b07658aa66530994171c14b794c9932f1e1b674f
Andrea Mourino <amourinodesousa@danielcastelao.org>
2025-03-13 13:19:25 +0100

    trabajando

---
+++ Boletin10/src/Trabajadores.java
@@ -0,0 +1,16 @@
+public class Trabajadores {
+
+
+    private String DNI;
+    private String nombre;
+    private int fechaingreso;
+
+
+
+    public String aCadea(){
+
+    }
+    public int gastosIngresos(){
+
+    }
+}
